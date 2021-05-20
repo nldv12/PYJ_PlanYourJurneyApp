1f8418987fe535cdb5563f10e05defc9075fd5eb
Nazar Majewski <nldv12@gmail.com>
2021-05-20 12:02:27 +0200

    dodana strona singleJourney

--- src/components/SelectedJourney/SelectedJourney.js
+++ src/components/SelectedJourney/SelectedJourney.js
@@ -3,27 +3,42 @@ import "./SelectedJourney.scss"
 import {InputNumber, InputCheckbox, InputSelect5} from "../MainCOMPONENTS/MainCOMPONENTS";
 import {FormLabel} from "../MainCOMPONENTS/MainCOMPONENTS";
 import {TotalPrice} from "../MainCOMPONENTS/MainCOMPONENTS";
+import {Link} from "react-router-dom";
 
 
 export const SelectedJourney = () => {
 
 
     return (
-        <div className={"SelectedJourney"}>
 
+            <div className={"SelectedJourney"}>
 
-            <div className={"selected_container"}>
-                <div className={"single_element destination"}>From: Gdańsk</div>
-                <div className={"picto picto1"}> </div>
-                <div className={"single_element destination"}>To: Amsterdam</div>
-                <div className={"single_element destination"}>F5000zł</div>
-                <div className={"single_element destination"}>Car</div>
-                <div className={"single_element destination"}>4 person</div>
-                <div className={"single_element destination"}>7 days</div>
+                <div className={"selected_container"}>
+                    <div className={"single_element"}>
+                        <div className={"picto picto_money"}> </div>
+                        <div className={"text price"}>5000zł</div>
+                    </div>
+                    <div className={"picto picto_car"}> </div>
+                    <div className={"single_element"}>
+                        <div className={"text city"}>Gdańsk</div>
+                        <div className={"picto picto_distance"}> </div>
+                        <div className={"text city"}>Amsterdam</div>
+                    </div>
+                    <div className={"last_line"}>
+                        <div className={"single_element"}>
+                            <div className={"picto picto_person"}> </div>
+                            <div className={"text person"}>4</div>
+                        </div>
+                        <div className={"single_element"}>
+                            <div className={"picto picto_day"}> </div>
+                            <div className={"text days"}>7 days</div>
+                        </div>
+                    </div>
+                </div>
+                <Link to="/singleAttractions" className={"btn btn_small"}>Check Attractions</Link>
 
             </div>
 
 
-        </div>
     )
 }
